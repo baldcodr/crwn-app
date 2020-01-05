70fb2dc1f36245eb27084321cae78f3c55bb5656
David Akuma <chukay360@gmail.com>
2020-01-05 06:31:00 +0100

    Updates

---
+++ precache-manifest.31f60b41580f71ed8f1a8a8f73df3fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347bef55a6f7b0b8a0e87e23e8b7c873",
+    "url": "/crwn-app/index.html"
+  },
+  {
+    "revision": "e05418a9b841e6faa700",
+    "url": "/crwn-app/static/css/main.1321e4b0.chunk.css"
+  },
+  {
+    "revision": "313408ae647ee0826e97",
+    "url": "/crwn-app/static/js/2.aee83b6d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crwn-app/static/js/2.aee83b6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e05418a9b841e6faa700",
+    "url": "/crwn-app/static/js/main.b9eb05be.chunk.js"
+  },
+  {
+    "revision": "bd169775bd61ce930ace",
+    "url": "/crwn-app/static/js/runtime-main.50224768.js"
+  }
+]);
