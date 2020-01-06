39fd1087902fb2db00a2b677c61bb5350e8158d4
David Akuma <chukay360@gmail.com>
2020-01-06 21:37:06 +0100

    shop page added and collection items in the shop-data rendered

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/home/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const JacketsPage = () => (
   <div>
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact="true" path="/" component={HomePage} />
         <Route path="/jackets" component={JacketsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
